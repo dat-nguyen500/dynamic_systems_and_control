1e25b5255f405b2326521942b37b6a7fadf5887f
Nguyễn Đức Đạt <151296540+dat-nguyen500@users.noreply.github.com>
2025-03-23 20:59:08 +0700

    Create tim_ham_truyen.m

---
+++ Bài tập lớn/1. Thu thập dữ liệu/tim_ham_truyen.m
@@ -0,0 +1,38 @@
+% Đọc dữ liệu từ file
+[V, theta] = readvars('nhan_dien.txt');
+[V1, theta1] = readvars('danh_gia.txt');
+
+% Thời gian lấy mẫu
+Ts = 0.1;
+
+% Tạo iddata
+data = iddata(theta, V, Ts);
+data1 = iddata(theta1, V1, Ts);
+
+% Thiết lập tùy chọn
+opt = tfestOptions('InitialCondition', 'zero');
+
+% Ước lượng hàm truyền (thử 2 cực, 1 zero)
+sys = tfest(data, 3, 0, opt)
+
+% Kiểm tra trên tập dữ liệu đánh giá
+figure(1);
+compare(data1, sys);
+
+% Lấy tử số và mẫu số
+num = [sys.Numerator, 0];
+den = [sys.Denominator(1) sys.Denominator(2) sys.Denominator(3) 0];
+
+% Tạo và tối giản hàm truyền
+G = tf(num, den);
+G = minreal(G)
+
+% Tính tốc độ ổn định
+omega = dcgain(G)
+
+% Vẽ đồ thị bước của hàm truyền
+figure(2);
+step(G);
+
+% Thông tin hàm bước
+stepinfo(G)
